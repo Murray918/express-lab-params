f50c12cd1f40cd1b1f49976d535c065d0a79dcb1
Jacob Werbin <jwerbin95@gmail.com>
2019-01-15 15:01:00 -0600

    Commit 2: Tip Calculator express application created.

--- server.js
+++ server.js
@@ -6,4 +6,12 @@ app.get('/greeting/', (request, response) => {
 app.get('/greeting/:name', (request, response) => {
 	response.send(`Hello, ${request.params.name}!`);
 });
+app.get('/tip/:total/:percentage', (request, resposne) => {
+	resposne.send(
+		`${calculateTip(request.params.total, request.params.percentage)}`
+	);
+});
+function calculateTip(total, percentage) {
+	return total * (percentage / 100);
+}
 app.listen(9000);
